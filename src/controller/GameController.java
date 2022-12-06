4aab2ce53f5a13c23047faee124101d62a54ff28
Jawad Makhoul <jawad.makhol@gmail.com>
2022-12-06 21:03:46 +0200

    GameController
    press button to change image...

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -4,6 +4,7 @@ import java.awt.event.MouseEvent;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.event.*;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -18,14 +19,19 @@ import javafx.stage.Stage;
 
 public class GameController {
 	
-	 @FXML
-	    private Button CI00;
+	
 	@FXML
     private ImageView S00,S01,S02,S03,S04,S05,S06,S07,S10,S11,S12,S13,S14,S15,S16,S17,S20,S21,S22,S23,S24,S25,S26,S27,S30,S31,S32,S33,S34,S35,S36,S37,S40,S41,S42,S43,S44,S45,S46,S47,S50,S51,S52,S53,S54,S55,S56,S57,S60,S61,S62,S63,S64,S65,S66,S67,S70,S71,S72,S73,S74,S75,S76,S77;
 
 	@FXML
     private ImageView I00,I01,I02,I03,I04,I05,I06,I07,I10,I11,I12,I13,I14,I15,I16,I17,I20,I21,I22,I23,I24,I25,I26,I27,I30,I31,I32,I33,I34,I35,I36,I37,I40,I41,I42,I43,I44,I45,I46,I47,I50,I51,I52,I53,I54,I55,I56,I57,I60,I61,I62,I63,I64,I65,I66,I67,I70,I71,I72,I73,I74,I75,I76,I77;
-    @FXML
+    
+	@FXML
+    private Button CI00;
+	@FXML
+    private Button CI01,CI02,CI03,CI04,CI05,CI06,CI07,CI10,CI11,CI12,CI13,CI14,CI15,CI16,CI17,CI20,CI21,CI22,CI23,CI24,CI25,CI26,CI27,CI30,CI31,CI32,CI33,CI34,CI35,CI36,CI37,CI40,CI41,CI42,CI43,CI44,CI45,CI46,CI47,CI50,CI51,CI52,CI53,CI54,CI55,CI56,CI57,CI60,CI61,CI62,CI63,CI64,CI65,CI66,CI67,CI70,CI71,CI72,CI73,CI74,CI75,CI76,CI77;
+	
+	@FXML
     private Button backBtn;
 
     @FXML
@@ -43,16 +49,20 @@ public class GameController {
     @FXML
     private AnchorPane screen;
 	
-    @FXML
-    void MoveTo(ActionEvent event) {
-    	Image image = new Image("/lib/king.png");
-    	if(CI00.isPressed()) {
-    		I00.setImage(image);
-    	}
+    
+    
+    public void MoveTo() {
+    	
+    		Image img = new Image(getClass().getResourceAsStream("/lib/king.png"));
+    		
+    		if(CI00.isPressed()==false)
+    		{System.out.print("vvvv");}
+    		I00.setImage(img);
+    	
     }
     
 	@FXML
-	private void back(ActionEvent event) throws IOException {
+	 private void back(ActionEvent event) throws IOException {
 		
 		Parent pane = FXMLLoader.load(getClass().getResource("/views/MainMenu.fxml"));
 		Scene scene = new Scene(pane);
