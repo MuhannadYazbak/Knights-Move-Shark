8ccbb439b819b43dd547efb58e5a15270bbf6bac
MuhannadYazbak <yazbakm@gmail.com>
2022-12-13 21:18:10 +0200

    fixed jar\bat file

--- runme.bat
+++ runme.bat
@@ -1,2 +1,1 @@
-java  --module-path "C:\Users\lover\Downloads\openjfx-19_windows-x64_bin-sdk\javafx-sdk-19\lib" --add-modules ALL-MODULE-PATH -jar runme.jar
-pause
+java  --module-path ".\openjfx-19_windows-x64_bin-sdk\javafx-sdk-19\lib" --add-modules ALL-MODULE-PATH -jar runme.jar
